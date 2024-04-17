dc29131750eb992f99ea1e0eeb628a0ad32bcb13
Lunamana00 <142918648+Lunamana00@users.noreply.github.com>
2024-04-17 22:01:43 +0900

    Add files via upload

--- 1463.py
+++ 1463.py
@@ -1,38 +1,28 @@
 num = int(input())
 
-def div3(a):
-    a = int(a/3)
-    return a
-
-def div2(a):
-    a = int(a/2)
-    return a
-
-def minus(a):
-    a = a -1
-    return a
-
 count = 0
 
-temp_list = [num]
-temp_list2 = []
+while True :
 
-while (1 not in temp_list):
+    if num ==1:
+        break
 
+    if num%3 == 0:
+        
+        num = num//3
+    
+    elif (num-1)%3 == 0 and num%4 != 0:
+
+        num = num-1
+    
+    elif num%2 == 0:
+
+        num = num//2
+    
+    else:
+        num = num-1
+    
     count += 1
 
-    for i in temp_list:
-        
-        if i%3 == 0:
-            temp_list2.append(div3(i))
-        if i%2 == 0:
-            temp_list2.append(div2(i))
-        temp_list2.append(minus(i))
-
-    temp_list = []
-    for s in temp_list2:
-        temp_list.append(s)
-
-    temp_list2 = []
-
 print(count)
+
