ab16e49268d053fb82d8341f27984aac0ae23413
김태현 <142918648+Lunamana00@users.noreply.github.com>
2025-03-29 16:19:08 +0900

    1021

--- BaaaaaaaarkingDog/0x07/0x07/1021.cpp
+++ BaaaaaaaarkingDog/0x07/0x07/1021.cpp
@@ -1,19 +1,65 @@
 #include <iostream>
-#include <algorithm>
-
-#include <vector>
-#include <string>
-
 #include <deque>
-
+#include <vector>
 using namespace std;
 
-int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+void Oper_1(deque<int>& deq) {
+    deq.pop_front();
+}
 
-    deque<int> deq;
-    int N, x;
-    string order, out_str;
-    cin >> N;
+void Oper_2(deque<int>& deq) {
+    deq.push_back(deq.front());
+    deq.pop_front();
+}
+
+void Oper_3(deque<int>& deq) {
+    deq.push_front(deq.back());
+    deq.pop_back();
+}
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    deque<int> dq;
+    vector<int> targets(m);
+
+    for (int i = 1; i <= n; i++) {
+        dq.push_back(i);
+    }
+
+    for (int i = 0; i < m; i++) {
+        cin >> targets[i];
+    }
+
+    int count = 0;
+
+    for (int i = 0; i < m; i++) {
+        int target = targets[i];
+        int idx = 0;
+
+        for (int j = 0; j < dq.size(); j++) {
+            if (dq[j] == target) {
+                idx = j;
+                break;
+            }
+        }
+
+        if (idx <= dq.size() / 2) {
+            while (dq.front() != target) {
+                Oper_2(dq);
+                count++;
+            }
+        }
+        else {
+            while (dq.front() != target) {
+                Oper_3(dq);
+                count++;
+            }
+        }
+        Oper_1(dq);
+    }
+
+    cout << count << '\n';
+    return 0;
 }
