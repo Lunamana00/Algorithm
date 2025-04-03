d49c42089ab4061657a1a43a448a28f7a8416140
김태현 <142918648+Lunamana00@users.noreply.github.com>
2025-04-04 02:06:34 +0900

    1182

--- BaaaaaaaarkingDog/0x0c/1182.cpp
+++ BaaaaaaaarkingDog/0x0c/1182.cpp
@@ -4,19 +4,34 @@
 
 using namespace std;
 
-int N, S, _count;
+int N, S, _count=0;
+int v_input[30];
 
-void func(int Tsum) {
-	if (Tsum == S) {
+
+void func(int cur,int Tsum) {
+	if (cur== N) {
+		if(S == Tsum)
 		_count++;
 		return;
 	}
 
-
+	func(cur+1, Tsum);
+	func(cur + 1, Tsum + v_input[cur]);
 }
 
 int main() {
-	cin >> N >> S;
-	cout << _count;
 
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int x;
+	cin >> N >> S;
+	for (int i = 0;i < N;i++) {
+		cin >> v_input[i];
+	}
+	func(0,0);
+	if (S == 0) {
+		_count--;
+	}
+	cout << _count;
 }
