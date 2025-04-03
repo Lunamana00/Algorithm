ef55f6f076982f3abe5845e4c7b65c459cd95bca
김태현 <142918648+Lunamana00@users.noreply.github.com>
2025-04-03 14:57:47 +0900

    9663

---
+++ BaaaaaaaarkingDog/0x0c/1182.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+
+using namespace std;
+
+int N, S, _count;
+
+void func(int Tsum) {
+	if (Tsum == S) {
+		_count++;
+		return;
+	}
+
+
+}
+
+int main() {
+	cin >> N >> S;
+	cout << _count;
+
+}
--- BaaaaaaaarkingDog/0x0c/9663.cpp
+++ BaaaaaaaarkingDog/0x0c/9663.cpp
@@ -1,14 +1,40 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-void func() {
+int Q_Count = 0;
+int N;
+vector<int> isused1(40);
+vector<int> isused2(40);
+vector<int> isused3(40);
 
+void func(int K) {
+	if (N == K) {
+		Q_Count++;
+		return;
+	}
+
+	for (int row = 0; row < N; row++) {
+		if (isused1[row] || isused2[row+K]||isused3[K-row+N-1]) {
+			continue;
+		}
+		isused1[row] = 1;
+		isused2[row+K] = 1;
+		isused3[K - row + N - 1] = 1;
+		func(K + 1);
+		isused1[row] = 0;
+		isused2[row + K] = 0;
+		isused3[K - row + N - 1] = 0;
+
+	}
 }
 
 
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	
+	cin >> N;
+	func(0);
+	cout << Q_Count;
 }
